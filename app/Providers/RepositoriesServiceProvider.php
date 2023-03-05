26b8dedd9959749e955793a786a3f0b89d486e39
Ari Rifani <ari.rifani0333@gmail.com>
2023-03-05 21:15:30 +0700

    add: register product image repository in repository service provider

--- app/Providers/RepositoriesServiceProvider.php
+++ app/Providers/RepositoriesServiceProvider.php
@@ -4,8 +4,11 @@ namespace App\Providers;
 
 use App\Repositories\Category\CategoryRepository;
 use App\Repositories\Category\EloquentCategoryRepository;
+use App\Repositories\CategoryProduct\CategoryProductRepository;
 use App\Repositories\Product\EloquentProductRepository;
 use App\Repositories\Product\ProductRepository;
+use App\Repositories\ProductImage\EloquentProductImageRepository;
+use App\Repositories\ProductImage\ProductImageRepository;
 use Illuminate\Support\ServiceProvider;
 
 class RepositoriesServiceProvider extends ServiceProvider
@@ -17,6 +20,8 @@ class RepositoriesServiceProvider extends ServiceProvider
     {
         $this->app->bind(CategoryRepository::class, EloquentCategoryRepository::class);
         $this->app->bind(ProductRepository::class, EloquentProductRepository::class);
+        $this->app->bind(CategoryProductRepository::class, EloquentProductRepository::class);
+        $this->app->bind(ProductImageRepository::class, EloquentProductImageRepository::class);
     }
 
     /**
