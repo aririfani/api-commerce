0023d3512d2a7add517c8d7510bbfaaac6ba813c
Ari Rifani <ari.rifani0333@gmail.com>
2023-03-05 16:24:44 +0700

    feat: add product endpoint

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ProductRequest;
+use App\Http\Resources\ProductResource;
+use App\Services\Product\ProductService;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+/**
+ * class ProductController
+ * @package App\Http\Controllers\Api
+ */
+class ProductController extends Controller
+{
+    /**
+     * @var $service
+     */
+    private $service;
+
+    /**
+     * @param \App\Services\Product\ProductService $categoryService
+     */
+    public function __construct(ProductService $productService)
+    {
+        $this->service = $productService;
+    }
+
+    /**
+     * Display a listing of the resource.
+     */
+    public function index(Request $request)
+    {
+        $data = $this->service->getAllWithPaginate($request->get('limit') ?: 10);
+
+        return ProductResource::collection($data);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     * @param \App\Http\Requests\ProductRequest $request
+     * @return \App\Http\Resources\ProductResource
+     */
+    public function store(ProductRequest $request)
+    {
+        $data = $this->service->create($request->only('name','description','enable'));
+
+        return new ProductResource($data);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        $data = $this->service->findById($id);
+
+        if (!$data) {
+            throw new NotFoundHttpException();
+        }
+
+        return new ProductResource($data);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        $data = $this->service->update($request->only('name','description','enable'), $id);
+
+        return new ProductResource($data);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        $this->service->delete($id);
+
+        return response()->json(['message' => 'success', 'status_code' => 200]);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\CategoryController;
+use App\Http\Controllers\Api\ProductController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,3 +24,9 @@ Route::patch('/v1/category/update/{id}',[CategoryController::class,'update']);
 Route::get('/v1/category',[CategoryController::class,'index']);
 Route::post('/v1/category',[CategoryController::class, 'store']);
 Route::get('/v1/category/{id}',[CategoryController::class,'show']);
+
+Route::patch('/v1/product/update/{id}',[ProductController::class,'update']);
+Route::get('/v1/product',[ProductController::class,'index']);
+Route::post('/v1/product',[ProductController::class, 'store']);
+Route::get('/v1/product/{id}',[ProductController::class,'show']);
+Route::delete('/v1/product/{id}',[ProductController::class, 'destroy']);
