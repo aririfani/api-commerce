064bb10c1e555d528ea5207bcfca730da4f7581d
Ari Rifani <ari.rifani0333@gmail.com>
2023-03-06 09:55:16 +0700

    fix: add delete endpoint cateogry

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,11 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::patch('/v1/category/update/{id}',[CategoryController::class,'update']);
+Route::patch('/v1/category/{id}',[CategoryController::class,'update']);
 Route::get('/v1/category',[CategoryController::class,'index']);
 Route::post('/v1/category',[CategoryController::class, 'store']);
 Route::get('/v1/category/{id}',[CategoryController::class,'show']);
+Route::delete('/v1/category/{id}',[CategoryController::class,'destroy']);
 
 Route::patch('/v1/product/update/{id}',[ProductController::class,'update']);
 Route::get('/v1/product',[ProductController::class,'index']);
